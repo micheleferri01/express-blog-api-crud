d2428520f4cb2b94c769103c56ac7fdecc564845
micheleferri01 <micheleferri095@gmail.com>
2026-03-03 17:18:11 +0100

    bonus

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -13,14 +13,23 @@ const index = (req, res) => {
             return false;
         })
     }
-    res.json(postsCopy);
+    res.json(
+        {
+            success: true,
+            result: postsCopy
+        }
+    );
 };
 const show = (req, res) => {
     const id = parseInt(req.params.id);
     // res.send(`Visualizzato post ${req.params.id}`);
     const post = posts.filter((post) => post.id === id);
 
-    post.length != 0 ? res.json(post) : res.status(404).send("Post non trovato.");
+    post.length != 0 ? res.json(post) : res.status(404).json(
+        { 
+        success: false,
+        message: "Post non trovato."
+    });
 
 
 };
@@ -38,8 +47,18 @@ const destroy = (req, res) => {
     const id = parseInt(req.params.id);
     const newPostsList = posts.filter((post) => post.id != id);
     console.log(newPostsList);
+    const deletedPost = posts.filter((post) => post.id === id);
+
+    deletedPost.length > 0? 
+    res.status(204): 
+    res.status(404).json(
+        {
+            success: false,
+            message: "il post che si vuole eliminare non esiste"
+        }
+    );
     
-    res.status(204).json();
+    
 
 };
 
